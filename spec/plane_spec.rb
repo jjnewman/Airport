5d00643865ed6db8ce861dce26d5305d428b1213
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-02-07 10:04:17 +0000

    plane spec file updated

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,5 +7,13 @@ let(:plane){Plane.new}
 it 'has a flying status when created' do
 	expect(plane).to be_flying
 end
+
+it 'should have a flying status after taking off' do
+	expect(plane.take_off!).to be_flying
+end
+
+it 'should not have a flying status after landing' do
+	expect(plane.land!).not_to be_flying
+end
 	
 end
